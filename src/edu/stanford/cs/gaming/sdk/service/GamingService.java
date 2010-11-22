4cdb7ce63892d0f37758c3fed11d22db503f74b0
Anthony Lai <aslai@ASLAI-MacBook-Pro.local>
2010-11-22 15:14:29 -0800

    Changed happyfeet to moovy

--- src/edu/stanford/cs/gaming/sdk/service/GamingService.java
+++ src/edu/stanford/cs/gaming/sdk/service/GamingService.java
@@ -47,7 +47,8 @@ public class GamingService extends Service implements LocationListener {
 //	private Intent broadcast=new Intent(BROADCAST_ACTION);	
 //	public static String gamingServer = "http://171.64.73.173:3000";
 //	public static String gamingServer = "http://colby.stanford.edu/gaming";
-	public final static String GAMING_SERVER = "https://happyfeet.heroku.com";
+//	public final static String GAMING_SERVER = "https://happyfeet.heroku.com";
+	public final static String GAMING_SERVER = "https://moovy.heroku.com";
 //	public static String gamingServer = "http://76.126.253.56:3000";
 	public final static String SDK_VERSION = "1";
 	
